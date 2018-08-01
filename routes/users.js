354b1a74f8c4350ba3e6f73b11f018afa8ba3ec5
Esteban Leon <eleon182@gmail.com>
2018-08-01 09:00:48 -0700

    update auth

--- routes/users.js
+++ routes/users.js
@@ -6,28 +6,36 @@ var LRU = require("lru-cache"),
     options = {
         max: 1000,
         //length: function(n, key) {
-            //return n
+        //return n
         //},
         maxAge: 1000 * 60 * 60 * 6
     },
     cache = LRU(options)
 
 router.post('/', function(req, res, next) {
-	console.log('Hook entry', req.body);
+    console.log('Hook entry', req.body);
     cache.set(new Date().toISOString(), req.body);
     res.json(req.body);
 });
 
 router.get('/', function(req, res, next) {
-	console.log('Hook history lookup', cache.length);
-    res.json(buildHistory());
+    if (req.headers['custom-auth'] === 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJiaHBoRGVhbGVyIiwibnM6diI6InYxIiwibnM6dSI6IjNiZjNjYzFhLTMxMGQtNDJmYi05MWNkLTJjOTMzYmQ0MmUwMSIsIm5zOnNnaSI6W10sImlzcyI6Imh0dHBzOlwvXC9zcGlyZW9uLmNvbSIsIm5zOmFwcCI6IjEzMzczZDRjLWI3MDAtNGE2ZS1iMzEyLWNiZTlmNWJjNzIzMyIsIm5zOnJvbGUiOlsiUk9MRV9ERUFMRVIiLCJST0xFX1VTRVIiXSwiZXhwIjoxNTMzMjI1NDc2LCJuczp0IjoiYmhwaERlYWxlcisxMzM3M2Q0Yy1iNzAwLTRhNmUtYjMxMi1jYmU5ZjViYzcyMzMiLCJuczpwb2wiOiI1N2NlZTc4MDFhOGM2NTUwMTQwMDAwMDciLCJuczpiIjoidmVoaWNsZUZpbmFuY2UiLCJuczphIjoiMTQ0Nzk3NDI5MjMyMEVUMDY0VEoifQ.KcoPlF-oQerxa_B_y79ROY5TPdU42vnBopOPRsDaIyGcz8zZf79_qdSTuj4n8aBbTzJp-LYrjOpUh-MNuJ_1aeQ6flMScJ3GO7o0nj6IWmijYnFFPWLRBAp78MY-IyQ3zwzpIhDiENwVfxg0RicnyDCpbvrx1aIm0xOPgfjI0fbw_CEBoFtZSmd6gjWsWLMX6bo0oZwGwxfx_LdYFeBhIqXryGQPiEeSgR_97GXHFjNo6l-54OfwwO6vm1hM3mJqHNKzP-ynny6CE3-TAkMquaDs7jqn7Z0KZh9tf1__P0Tqq1W1EaewuNWUuh05SVk5XqSU2kfW2rxznloTTcQZYg') {
+		console.log('Hook history lookup', cache.length);
+		res.json(buildHistory());
+    }
+
+	else {
+		res.status(401).json({
+			err: 'invalidCredentials'
+		});
+	}
 });
 
 function buildHistory() {
     var history = [];
     cache.forEach((value, key) => {
-		value = value || {};
-		value.hookEntryTimeStamp=  key;
+        value = value || {};
+        value.hookEntryTimeStamp = key;
         history.push(value);
     });
     return history;
